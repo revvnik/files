e64e20b02b916e8cb1273180d57fa023a827cf63
polygon6233 <nikitasitter@gmail.com>
2025-02-04 18:13:16 +0100

    Fixed website for PC

--- assets/js/decrypt-worker.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = async function ({ data }) {
-    const { filePath, key } = data;
-    try {
-        const decryptedImageUrl = await decryptFile(filePath, key);
-        self.postMessage({ success: true, decryptedImageUrl });
-    } catch (error) {
-        self.postMessage({ success: false, error: error.message });
-    }
-};
